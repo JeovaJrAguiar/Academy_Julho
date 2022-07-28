fd82b5fec02cf6dd3c8dcd8a9413d056daff5563
Gabriel Sousa <wgsousa12@gmail.com>
2022-07-28 14:00:13 -0300

    add fruta (DOM)

--- script.js
+++ script.js
@@ -1,45 +1,20 @@
-const frutas = [
-  { name: "Laranja", count: 5 },
-  { name: "Banana", count: 2 },
-  { name: "Maçã", count: 9 },
-  { name: "Uva", count: 4 },
-  { name: "Goiaba", count: 5 },
-];
+const plus = document.querySelector(".adicionar");
+const minus = document.querySelector(".remover");
+const valor = document.querySelector(".inputText");
 
-const mincount = 6;
+let value = 0;
 
-const frutasPertoDeAcabar = frutas.reduce((ac, item) => {
-  if (item.count < mincount) {
-    console.log(item.name);
+function add() {
+  value++;
+  valor.innerHTML = value;
+}
+
+function rem() {
+  if (value > 0) {
+    value--;
+    valor.innerHTML = value;
   }
-}, "");
+}
 
-console.log(frutasPertoDeAcabar); 
-
-// ERRO: ESTÁ RETORNANDO UM UNDEFINED NO FINAL
-
-
-
-
-///     EXERCICIOS
-
-// Descobrir quantas pessoas são do signo de cancer
-const pessoas = [
-  { name: "Pedro", sign: "Gemeos" },
-  { name: "Lucas", sign: "Cancer" },
-  { name: "Andre", sign: "Cancer" },
-  { name: "Julio", sign: "Sargitario" },
-  { name: "Luma", sign: "Aries" },
-  { name: "Alvaro", sign: "Cancer" },
-  { name: "Leticia", sign: "Cancer" },
-  { name: "Juliana", sign: "Aquario" },
-];
-
-const cancer = pessoas.reduce((acc, { sign }) => {
-  if (sign === "Cancer") {
-    acc += 1;
-  }
-  return acc;
-}, 0);
-
-console.log(cancer);
+plus.addEventListener("click", add);
+minus.addEventListener("click", rem);
